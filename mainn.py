a5f4cf3e9db28d60e533f3e56f2c117babb64fca
Alessandro Mendes <mendesalessandro763@gmail.com>
2024-04-03 17:06:10 -0300

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -1,1 +1,8 @@
-print('HELLO WORLD')
+LISTA = ["Python","Código","Dev"]
+
+try:
+    for i in range(2):
+     print("O indice {} é o elemento {}".format(i,lista[i]))
+
+except:
+   print('Indice esta fora do range')
