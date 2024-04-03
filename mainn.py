5cc727e6c72a7af79764d796956711be1438eae7
Alessandro Mendes <mendesalessandro763@gmail.com>
2024-04-03 17:26:19 -0300

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -1,8 +1,16 @@
-LISTA = ["Python","Código","Dev"]
+def multiplicacao(num1,num2):
+    resultado = num1 * num2
+    print('Resultado da multiplição: {}'.format(resultado))
 
-try:
-    for i in range(4):
-        print("O indice {} é o elemento {}".format(i,lista[i]))
+if __name__ == "main":
+    permanece = True
 
-except:
-   print('Indice esta fora do range')
+while permanece:
+    try:
+        num1 = int(input('Informe o primerio numero: '))
+        num2 = int(input('Informe o segundo numero: '))
+        permanece = False
+    except ValueError:
+        print('Entrada Invalida.Certifique-se de inserir numeros validos.')
+
+multiplicacao(num1,num2)
